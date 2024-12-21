05885a2ca6ca619d6ed8aa9a3a2a4264187063da
chldbwlsN <chldbwls92@naver.com>
2024-12-22 02:34:49 +0900

    [최유진N] - 회원가입 html 구성중 - header footer부분 완성
    #1

---
+++ src/test/java/com/chldbwls/spring/gameWeb/GameWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chldbwls.spring.gameWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
